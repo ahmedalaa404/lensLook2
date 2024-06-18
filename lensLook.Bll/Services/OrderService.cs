e646585a23ca0478749cf229a863c0e8dffcc1ed
ahmed alaa yassin <93731317+ahmedalaa404@users.noreply.github.com>
2024-06-18 08:01:03 +0300

    OrderServies

--- lensLook.Bll/Services/OrderService.cs
+++ lensLook.Bll/Services/OrderService.cs
@@ -2,6 +2,10 @@
 using lensLook.Dal.Context;
 using lensLook.Dal.Models;
 using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace lensLook.Bll.Services
 {
@@ -9,58 +13,68 @@ namespace lensLook.Bll.Services
     {
         private readonly LensLookDbContext _context;
 
-        public OrderService(LensLookDbContext Context)
+        public OrderService(LensLookDbContext context)
         {
-            _context = Context;
+            _context = context;
         }
 
         public async Task<Order> CreateOrderAsync(Order model)
         {
             await _context.Order.AddAsync(model);
-            var Basket = _context.BasketCustomers.FirstOrDefault(x => x.UserId == model.UserIdCreateOrder);
 
-            model.SubTotal = model.Items.Sum(x => x.Price * x.Quantity) + 10;
-            Basket.BasketItems.Clear();
-            _context.BasketCustomers.Update(Basket);
-            _context.SaveChanges();
+            var basket = _context.BasketCustomers
+                .Include(bc => bc.BasketItems)
+                .FirstOrDefault(bc => bc.UserId == model.UserIdCreateOrder);
+
+            if (basket == null)
+            {
+                throw new InvalidOperationException("Basket not found for the user.");
+            }
+
+            model.SubTotal = model.Items.Sum(item => item.Price * item.Quantity) + 10;
+
+            basket.BasketItems.Clear();
+            _context.BasketCustomers.Update(basket);
+
+            await _context.SaveChangesAsync();
+
             return model;
         }
 
-        public Order GetOrderById(int OrderId)
+        public Order GetOrderById(int orderId)
         {
-            var Order = _context.Order.Where(x => x.Id == OrderId).Include(x => x.Items).FirstOrDefault();
-
-            return Order;
+            return _context.Order
+                .Include(o => o.Items)
+                .FirstOrDefault(o => o.Id == orderId);
         }
 
-        public List<Order> GetOrdersForUser(string UserId)
+        public List<Order> GetOrdersForUser(string userId)
         {
-            return _context.Order.Where(x => x.UserIdCreateOrder == UserId).Include(x => x.Items).ToList();
+            return _context.Order
+                .Where(o => o.UserIdCreateOrder == userId)
+                .Include(o => o.Items)
+                .ToList();
         }
 
-
-
-
-        public bool RemoveOrder(int x)
+        public bool RemoveOrder(int orderId)
         {
             try
             {
-                _context.Order.Remove(GetOrderById(x));
+                var order = GetOrderById(orderId);
+                if (order == null)
+                {
+                    return false;
+                }
+
+                _context.Order.Remove(order);
                 _context.SaveChanges();
+
                 return true;
             }
             catch (Exception)
             {
                 return false;
-                throw;
             }
         }
-
-
-
-
-
-
-
     }
 }
